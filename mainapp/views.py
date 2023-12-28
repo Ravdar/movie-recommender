f96c9f3943a42d05b97558e8e34bb453413d418e
Ravdar <97836782+Ravdar@users.noreply.github.com>
2023-12-28 18:59:14 +0100

    working on file upload

--- mainapp/views.py
+++ mainapp/views.py
@@ -16,9 +16,12 @@ def main_view(request):
         start_time = time.time()
         prompt_form = UserPrompt(request.POST)
         if prompt_form.is_valid():
-            assistant = client.beta.assistants.create(name="Movie recommender", instructions='You are a movie expert. Your role is to recommend 5 movies, based on the data provided by an user. The output should be python list of dictionaries with keys: "Title", "Year", "Short description". The list should be named "movies". Do not write anything else, provide just a list.', model="gpt-3.5-turbo-1106")
+            file = client.files.create(file=open("C:\\Users\\Tomasz\\Desktop\\movie_recommender\\mainapp\\static\\already_seen_movies.txt", "rb"), purpose="assistants")
+            print(file)
+            print(file.id)
+            assistant = client.beta.assistants.create(name="Movie recommender", instructions='You are a movie expert. Generate a Python list of dictionaries named "movies" with keys: "Title", "Year", "Short description". Recommend 5 movies based on the data provided. Do not include any introductory text such as "Here are some movie recommendations for you:". Also, make sure not to recommend any movies that are already watched. Provide only the raw list "movies".', model="gpt-3.5-turbo-1106", tools=[{"type": "retrieval"}], file_ids=[file.id])
             thread = client.beta.threads.create()
-            prompt = prompt_form.cleaned_data["text"]
+            prompt = prompt_form.cleaned_data['text']
             message = client.beta.threads.messages.create(thread_id=thread.id, role="user", content=prompt)
             run = client.beta.threads.runs.create(thread_id=thread.id, assistant_id=assistant.id)
             while run.status == "queued" or run.status == "in_progress":
@@ -31,9 +34,11 @@ def main_view(request):
             time.sleep(1)
             messages = client.beta.threads.messages.list(thread_id=thread.id)
             string_data = messages.data[0].content[0].text.value
+            print(string_data)
             start_index = string_data.find('[')
             end_index = string_data.rfind(']') + 1
             movies_list_str = string_data[start_index:end_index]
+            print(movies_list_str)
             response = ast.literal_eval(movies_list_str)
             print(response)
             print(f"Before imdb data {time.time() - start_time}")
