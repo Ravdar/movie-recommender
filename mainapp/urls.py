49577688af29f5e2bff06bf21176ea8e8e5ce651
Ravdar <97836782+Ravdar@users.noreply.github.com>
2024-02-04 18:23:44 +0100

    Refactoring + responsive design

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from . import views
 
 app_name = "mainapp"
-urlpatterns = [path("", views.main_view, name="main_view_url"),
-               path("results/", views.main_view, name="results_view_url")]
+urlpatterns = [path("", views.main_view, name="main_view_url"),]
