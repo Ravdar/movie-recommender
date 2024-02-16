3be7035a177d0aa54574082f3612b6cacb7b1dae
Ravdar <97836782+Ravdar@users.noreply.github.com>
2024-02-16 19:59:51 +0100

    styling feedback form, working on footer

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Movie, Recommendation
+from .models import Movie, Recommendation, Feedback
 
 admin.site.register(Movie)
 admin.site.register(Recommendation)
+admin.site.register(Feedback)
 
 # Register your models here.
