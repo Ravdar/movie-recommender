0912049e5742e94bf43bbeb9356ac21ec3c0b498
ravdar <8squeex8@gmail.com>
2024-05-08 17:51:05 +0200

    Deleted unnecessary files

--- mainapp/views.py
+++ mainapp/views.py
@@ -15,9 +15,6 @@ import os
 
 def main_view(request):
     api_key = os.environ.get("API_KEY")
-    print(api_key)
-#     client = OpenAI(
-#   api_key=("sk-proj-KQnRMn5E3IW6Huzxvs2lT3BlbkFJenAWXP7GCyshYh2D6IFY"))
     client = OpenAI(api_key=api_key)
     response = ""
     if request.method =="POST":
@@ -30,46 +27,9 @@ def main_view(request):
             file_ids = []
             instructions = 'You are a movie expert. Your role is to recommend 5 (five) movies, based on the data provided by an user.Please response with python list of dictionaries named "movies" with keys: "Title", "Year", "Plot short description".  No salutes, no explanations, no thank you, nothing other than the specified python list.'
             prompt_additional_info = " Please remember to not write any additional text in a response, provide just a list of 5 movies."
-
-            if prompt_form.cleaned_data["gpt_4"]:
-                assistant_id = 'asst_6bqZAqHpKP48jTaxrCSlbbxL' # 'asst_sKuRYRpYQWM6VigUHTnFzUhk'
-                # file_id = 'file-E7IBRtoF7uImZmH9kU36urA1'
-                tools = [{"type": "retrieval"}]
-                file_ids = [file_id]
-            if prompt_form.cleaned_data["only_watchlist"]:
-                assistant_id = 'asst_sKuRYRpYQWM6VigUHTnFzUhk'
-                # file_id = 'file-6J32Lw7YXCBQyIuYCA1tRAsH'
-                tools = [{"type": "retrieval"}]
-                file_ids = [file_id]
-
-            elif prompt_form.cleaned_data["without_seen"]:
-                assistant_id = 'asst_sKuRYRpYQWM6VigUHTnFzUhk'
-                # file_id = 'file-qWa8QHBYup9GjeskZCHtvlFh'
-                tools = [{"type": "retrieval"}]
-                file_ids = [file_id]
-
-            # Checking streaming services
-            selected_platforms = []
-            for field_name, field in prompt_form.fields.items():
-                if isinstance(field, forms.BooleanField) and field_name not in ['without_seen', 'only_watchlist', 'gpt_4']:
-                    if prompt_form.cleaned_data[field_name]:
-                        selected_platforms.append(field.label)
-            selected_platforms_str = ""
-
-            if selected_platforms == [] or len(selected_platforms) == 7:
-                pass
-            else:
-                assistant_id = 'asst_6bqZAqHpKP48jTaxrCSlbbxL'#'asst_sKuRYRpYQWM6VigUHTnFzUhk'
-                # file_id = 'file-E7IBRtoF7uImZmH9kU36urA1'
-                tools = [{"type": "retrieval"}]
-                file_id = [file_id]
-                prompt_additional_info = f' available on of these streaming platforms: {selected_platforms} .Please remember to not write any additional text in a response, provide just a list.'
-
-
             assistant = client.beta.assistants.update(assistant_id=assistant_id, tools=tools, instructions=instructions)
             thread = client.beta.threads.create()
             prompt = prompt_form.cleaned_data['text'] + prompt_additional_info
-            print(prompt)
             message = client.beta.threads.messages.create(thread_id=thread.id, role="user", content=prompt)
             run = client.beta.threads.runs.create(thread_id=thread.id, assistant_id=assistant_id)
             while run.status == "queued" or run.status == "in_progress":
@@ -81,7 +41,6 @@ def main_view(request):
             time.sleep(1)
             messages = client.beta.threads.messages.list(thread_id=thread.id)
             string_data = messages.data[0].content[0].text.value.replace("```python", "").replace("```","")
-            print(string_data)
             start_index = string_data.find('[')
             end_index = string_data.rfind(']') + 1
             movies_list_str = string_data[start_index:end_index]
@@ -99,29 +58,21 @@ def main_view(request):
                     rating = db_movie.rating
                     full_streaming = db_movie.streaming_services
                     try:
-                        streaming = full_streaming["US"]["flatrate"]
-                        print(streaming)
+                        streaming = full_streaming["US"]["flatrate"]        
                         streaming_services = [f"https://image.tmdb.org/t/p/original{platform['logo_path']}" for platform in streaming]
-                        print(streaming_services)
                     except:
                         streaming_services = []
                     try:
                         renting = full_streaming["US"]["rent"]
-                        print(renting)
                         renting_services = [f"https://image.tmdb.org/t/p/original{platform['logo_path']}" for platform in renting]
-                        print(renting_services)
                     except:
                         renting_services = []
                     try:
                         buying = full_streaming["US"]["buy"]
-                        print(buying)
                         buying_services = [f"https://image.tmdb.org/t/p/original{platform['logo_path']}" for platform in buying]
-                        print(buying_services)
                     except:
                         buying_services = []
                 else:
-                    print(movie)
-                    print("NOT EXISTS")
                     movie_info = get_movie_info_tmdb(movie_title, movie_year)
                     movie_link = movie_info["TMDB link"]
                     poster_link = movie_info["Poster"]
@@ -129,21 +80,15 @@ def main_view(request):
                     length = movie_info["Length"]
                     full_streaming = movie_info["Streaming"]
                     try:
-                        print(f'Streaming: {full_streaming["US"]["flatrate"]}')
                         streaming_services = [f"https://image.tmdb.org/t/p/original{platform['logo_path']}" for platform in full_streaming["US"]["flatrate"]]
-                        print(streaming_services)
                     except:
                         streaming_services = []
                     try:
-                        print(f'Renting: {full_streaming["US"]["rent"]}')
                         renting_services = [f"https://image.tmdb.org/t/p/original{platform['logo_path']}" for platform in full_streaming["US"]["rent"]]
-                        print(renting_services)
                     except:
                         renting_services = []
                     try:
-                        print(f'Buying: {full_streaming["US"]["buy"]}')
                         buying_services = [f"https://image.tmdb.org/t/p/original{platform['logo_path']}" for platform in full_streaming["US"]["buy"]]
-                        print(buying_services)
                     except:
                         buying_services = []                                                
                     db_movie = Movie(title=movie_title, year=movie["Year"], length=length, poster_url = poster_link, tmdb_link=movie_link, streaming_services = full_streaming, rating=rating, last_update=datetime.today().date())
@@ -158,15 +103,14 @@ def main_view(request):
                 movie["Description"] = movie["Plot short description"]
                 recommendation.recommended_movies.add(db_movie)
             processing_time = timedelta(seconds=(datetime.now() - start_time).total_seconds())
-            print(type(processing_time))
             recommendation.response_time = processing_time
             recommendation.save()
             prompt_form = UserPrompt()
-            return render(request, "mainapp/refactored.html", {"prompt_form":prompt_form, "prompt":prompt, "response":response, "processing_time":processing_time})
+            return render(request, "mainapp/main_view.html", {"prompt_form":prompt_form, "prompt":prompt, "response":response, "processing_time":processing_time})
     else:
         prompt_form = UserPrompt()
         welcome_message = "Hello! MovieNeon, the intelligent movie matchmaker, is at your service. Share your prompts, and let MovieNeon craft a personalized movie playlist based on your preferences. Begin typing your prompts now!"
-    return render(request, "mainapp/refactored.html", {"prompt_form":prompt_form,"welcome_message":welcome_message})
+    return render(request, "mainapp/main_view.html", {"prompt_form":prompt_form,"welcome_message":welcome_message})
 
 def about_view(request):
     if request.method == "POST":
